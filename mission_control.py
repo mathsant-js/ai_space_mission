507ea44a4e8869709826a61251856b599e6ebd5b
mathsant-js <matheus.santana114@etec.sp.gov.br>
2026-05-27 15:13:38 -0300

    refactor: adicionando as matrizes de dados

---
+++ mission_control.py
@@ -0,0 +1,16 @@
+dados_missao = [
+    [24, 92, 88, 96, 90],
+    [27, 80, 72, 94, 85],
+    [31, 65, 58, 91, 70],
+    [36, 42, 38, 87, 55],
+    [39, 28, 19, 78, 35],
+    [34, 55, 32, 82, 50]
+]
+
+areas_monitoradas = [
+    "Temperatura interna",
+    "Comunicação com a base",
+    "Sistema de energia",
+    "Suporte de oxigênio",
+    "Estabilidade operacional"
+]
