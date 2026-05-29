66feb0f12f77b4443f010fe78d3b2e04f85c9325
Matheus Santana <130666046+mathsant-js@users.noreply.github.com>
2026-05-29 01:54:00 -0300

    feat: & fix: analise de informações individuais de cada ciclo foi consertada, cálculo de pontuação de cada ciclo foi adicionado e classificação de cada ciclo foi adicionada
    - lista_risco representa o nível de risco de cada estatística da missão
    - atualizar_lista_risco(i), atualiza os valores de risco na lista_risco a cada ciclo para analisar cada informação separadamente
    - somar_risco_ciclo(soma_risco), soma a pontuação de risco de cada ciclo
    - classificar_ciclo(soma_risco), classifica o risco de cada ciclo
    - as funções de analisar foram consertadas para analisar as informações separadamente e retornar a string de risco da informação
    - as funções de somar risco de informação servem para calcular o risco de cada nível das informações

--- mission_control.py
+++ mission_control.py
@@ -1,50 +1,116 @@
+risco_temperatura = 0
+risco_comunicacao = 0
+risco_bateria = 0
+risco_oxigenio = 0
+risco_estabilidade = 0
+
+lista_risco = [risco_temperatura, risco_comunicacao, risco_bateria, risco_oxigenio, risco_estabilidade]
+
 def exibir_introducao_missao():
     print("============================================================")
     print("AI SPACE MISSION")
     print("============================================================")
     print("Missão: Moon Test Ômega")
     print("Equipe: Equipe Alpha")
-    print(f"Quantidade de Ciclos Analisados: 6")
+    print(f"Quantidade de Ciclos Analisados: {len(dados_missao)}")
     print()
 
 def percorrer_ciclo_missao():
-    risco_temperatura = 0
-    risco_comunicacao = 0
-    risco_bateria = 0
-    risco_oxigenio = 0
-    risco_estabilidade = 0
     for i in range(len(dados_missao)):
-        risco_temperatura += analisar_temperatura(dados_missao[i][0])
-        risco_comunicacao += analisar_comunicacao(dados_missao[i][1])
-        risco_bateria += analisar_bateria(dados_missao[i][2])
-        risco_oxigenio += analisar_oxigenio(dados_missao[i][3])
-        risco_estabilidade += analisar_estabilidade(dados_missao[i][4])
-        print(f"Temperatura: {dados_missao[i][0]} °C | {calcular_pontuacao_risco(risco_temperatura)}")
-        print(f"Comunicação: {dados_missao[i][1]}% | {calcular_pontuacao_risco(risco_comunicacao)}")
-        print(f"Bateria: {dados_missao[i][2]}% | {calcular_pontuacao_risco(risco_bateria)}")
-        print(f"Oxigênio: {dados_missao[i][3]}% | {calcular_pontuacao_risco(risco_oxigenio)}")
-        print(f"Estabilidade: {dados_missao[i][4]}% | {calcular_pontuacao_risco(risco_estabilidade)}")
+        global lista_risco
+        atualizar_lista_risco(i)
+        soma_risco = 0
+        soma_risco += somar_risco_ciclo(soma_risco)
+        print(f"CICLO {i + 1}")
+        print("------------------------------------------------------------")
+        print(f"Temperatura: {dados_missao[i][0]} °C | {analisar_temperatura(dados_missao[i][0])}")
+        print(f"Comunicação: {dados_missao[i][1]}% | {analisar_comunicacao(dados_missao[i][1])}")
+        print(f"Bateria: {dados_missao[i][2]}% | {analisar_bateria(dados_missao[i][2])}")
+        print(f"Oxigênio: {dados_missao[i][3]}% | {analisar_oxigenio(dados_missao[i][3])}")
+        print(f"Estabilidade: {dados_missao[i][4]}% | {analisar_estabilidade(dados_missao[i][4])}")
+        print()
+        print(f"Pontuação de risco do ciclo: {soma_risco}")
+        print(f"Classificação do ciclo: {classificar_ciclo(soma_risco)}")
         print()
 
-def calcular_pontuacao_risco(pontuacao):
-    match pontuacao:
-        case _ if pontuacao < 3:
-            return "NORMAL"
-        case _ if pontuacao < 6:
-            return "ATENÇÃO"
+def atualizar_lista_risco(i):
+    lista_risco[0] = somar_risco_temperatura(dados_missao[i][0])
+    lista_risco[1] = somar_risco_comunicacao(dados_missao[i][1])
+    lista_risco[2] = somar_risco_bateria(dados_missao[i][2])
+    lista_risco[3] = somar_risco_oxigenio(dados_missao[i][3])
+    lista_risco[4] = somar_risco_estabilidade(dados_missao[i][4])
+
+def somar_risco_ciclo(soma_risco):
+    for risco in lista_risco:
+        soma_risco += risco
+    return soma_risco
+
+def classificar_ciclo(soma_risco):   
+    match soma_risco:
+        case _ if soma_risco < 3:
+            return "MISSÃO ESTÁVEL"
+        case _ if soma_risco < 6:
+            return "MISSÃO EM ATENÇÃO"
         case _:
-            return "CRÍTICO"
+            return "MISSÃO CRÍTICA"
 
 def analisar_temperatura(temperatura):
+    match temperatura:
+        case _ if temperatura < 18 or temperatura > 30 and temperatura <= 35:
+            return "ATENÇÃO"
+        case _ if temperatura > 35:
+            return "CRÍTICO"
+        case _:
+            return "NORMAL"
+
+def analisar_comunicacao(comunicacao):
+    match comunicacao:
+        case _ if comunicacao < 30:
+            return "CRÍTICO"
+        case _ if comunicacao <= 59:
+            return "ATENÇÃO"
+        case _:
+            return "NORMAL"
+        
+def analisar_bateria(bateria):
+    match bateria:
+        case _ if bateria < 20:
+            return "CRÍTICO"
+        case _ if bateria <= 49:
+            return "ATENÇÃO"
+        case _:
+            return "NORMAL"
+        
+def analisar_oxigenio(oxigenio):
+    match oxigenio:
+        case _ if oxigenio < 80:
+            return "CRÍTICO"
+        case _ if oxigenio <= 89:
+            return "ATENÇÃO"
+        case _:
+            return "NORMAL"
+        
+def analisar_estabilidade(estabilidade):
+    match estabilidade:
+        case _ if estabilidade < 40:
+            return "CRÍTICO"
+        case _ if estabilidade <= 69:
+            return "ATENÇÃO"
+        case _:
+            return "NORMAL"
+
+def somar_risco_temperatura(temperatura):
     risco = 0
     match temperatura:
         case _ if temperatura < 18 or temperatura > 30 and temperatura <= 35:
             risco += 1
-        case _:
+        case _ if temperatura > 35:
             risco += 2
+        case _:
+            risco += 0
     return risco
 
-def analisar_comunicacao(comunicacao):
+def somar_risco_comunicacao(comunicacao):
     risco = 0
     match comunicacao:
         case _ if comunicacao < 30:
@@ -53,7 +119,7 @@ def analisar_comunicacao(comunicacao):
             risco += 1
     return risco
 
-def analisar_bateria(bateria):
+def somar_risco_bateria(bateria):
     risco = 0
     match bateria:
         case _ if bateria < 20:
@@ -62,7 +128,7 @@ def analisar_bateria(bateria):
             risco += 1
     return risco
 
-def analisar_oxigenio(oxigenio):
+def somar_risco_oxigenio(oxigenio):
     risco = 0
     match oxigenio:
         case _ if oxigenio < 80:
@@ -71,7 +137,7 @@ def analisar_oxigenio(oxigenio):
             risco += 1
     return risco
 
-def analisar_estabilidade(estabilidade):
+def somar_risco_estabilidade(estabilidade):
     risco = 0
     match estabilidade:
         case _ if estabilidade < 40:
